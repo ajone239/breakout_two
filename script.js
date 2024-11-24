3efe25adaf1d8b0772bb6436d59d94a773928bc3
Austin Jones <ajones53.aj@gmail.com>
2024-11-24 17:27:45 -0500

    paddle draws

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ document.addEventListener("gesturestart", function(e) {
     e.preventDefault();
 });
 
+let paddle;
+
 function setup() {
     createCanvas(document.body.clientWidth, document.body.clientHeight);
-    background("pink");
+
+    paddle = new Rectangle(
+        width * (1 / 2),
+        height * (9 / 10),
+        width * (1 / 6),
+        height * (1 / 25)
+    )
 }
 
 function draw() {
-    // credit
+    background("pink");
+
+    // title
     textAlign(CENTER, CENTER);
     text("Breakout", width / 2, 20);
 
@@ -18,14 +28,13 @@ function draw() {
             handleInteraction(touch.x, touch.y)
         }
     }
+
+    paddle.show()
 }
 
 function handleInteraction(x, y) {
-    push()
-    console.log("handle")
-    fill(0)
-    ellipse(x, y, 10, 10)
-    pop()
+    _ = y
+    paddle.x = x - (paddle.w / 2);
 }
 
 function mousePressed() {
@@ -36,4 +45,17 @@ function mousePressed() {
 function mouseDragged() {
     console.log("mouse")
     handleInteraction(mouseX, mouseY)
+}
+
+class Rectangle {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    show() {
+        rect(this.x, this.y, this.w, this.h)
+    }
 }
